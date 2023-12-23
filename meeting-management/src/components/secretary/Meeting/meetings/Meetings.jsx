5ed40e596c9528fcf913e49d6a2418edd5d3ea0a
Ali Khaled <alielsaadany94@gmail.com>
2023-12-24 00:33:35 +0200

    meetings page

--- meeting-management/src/components/secretary/Meeting/meetings/Meetings.jsx
+++ meeting-management/src/components/secretary/Meeting/meetings/Meetings.jsx
@@ -1,80 +1,171 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { DataGrid } from "@mui/x-data-grid";
 import Box from "@mui/material/Box";
 import "./meetings.css";
+import $ from "jquery";
+import { getRowsStateFromCache } from "@mui/x-data-grid/hooks/features/rows/gridRowsUtils";
+import { getGridRowElement, getRowEl } from "@mui/x-data-grid/utils/domUtils";
 export default function Meetings() {
+  useEffect(() => {
+    $(".tableColumns *").contextmenu((e) => {
+      e.preventDefault();
+    });
+    $(document).click(() => {
+      $("#context-menu").css({ display: "none" });
+    });
+  }, []);
+
   let [meetingsRows, setMeetingsRows] = useState([
     {
-      id: 1,
+      id: 3341,
       Date: "20-10-2025",
       Time: "3:00",
-      Person: "Ali",
+      Person: "Ali elsaadany",
       Topic: "I dont know",
       Address: "hy25a",
       Notes: "lol",
       Area: "Inside",
+      Status: "not done",
     },
     {
       id: 2,
       Date: "10-12-2024",
       Time: "12:00",
-      Person: "Mustafa",
+      Person: "Mustafa salem",
       Topic: "Lol lol odsasdakijf",
       Address: "el4ar3",
       Notes: "lol",
       Area: "Outside",
+      Status: "done",
     },
     {
       id: 3,
       Date: "10-05-2024",
       Time: "2:00",
-      Person: "Qadry",
+      Person: "Qadry omar",
       Topic: "Modo3 mohom",
       Address: "hy25a",
       Notes: "anta mal y3am lorem12fsd",
       Area: "Inside",
+      Status: "done",
     },
     {
       id: 4,
       Date: "10-05-2024",
       Time: "2:00",
-      Person: "Qadry",
+      Person: "Mahmoud khaled",
       Topic: "Modo3 mohom",
       Address: "hy25a",
       Notes: "anta mal y3am lorem12fsd",
       Area: "Inside",
+      Status: "done",
     },
     {
       id: 5,
       Date: "10-05-2024",
       Time: "2:00",
-      Person: "Qadry",
+      Person: "Pop",
       Topic: "Modo3 mohom",
       Address: "hy25a",
       Notes: "anta mal y3am lorem12fsd",
       Area: "Inside",
+      Status: "cancelled",
     },
     {
       id: 6,
       Date: "10-05-2024",
       Time: "2:00",
-      Person: "Qadry",
+      Person: "Taha",
       Topic: "Modo3 mohom",
       Address: "hy25a",
       Notes: "anta mal y3am lorem12fsd",
       Area: "Inside",
+      Status: "not done",
     },
     {
       id: 7,
       Date: "10-05-2024",
       Time: "2:00",
-      Person: "Qadry",
+      Person: "Ali",
+      Topic: "Modo3 mohom",
+      Address: "hy25a",
+      Notes: "anta mal y3am lorem12fsdxxxxxxxxxas as asmjfkjfamisdk fsaokfjad",
+      Area: "Inside",
+      Status: "not done",
+    },
+    {
+      id: 8,
+      Date: "10-05-2024",
+      Time: "2:00",
+      Person: "Salah",
       Topic: "Modo3 mohom",
       Address: "hy25a",
       Notes: "anta mal y3am lorem12fsd",
       Area: "Inside",
+      Status: "not done",
+    },
+    {
+      id: 9,
+      Date: "10-05-2024",
+      Time: "2:00",
+      Person: "Nabil",
+      Topic: "Modo3 mohom",
+      Address: "hy25a",
+      Notes: "anta mal y3am lorem12fsd",
+      Area: "Inside",
+      Status: "not done",
+    },
+    {
+      id: 10,
+      Date: "10-05-2024",
+      Time: "2:00",
+      Person: "Omar",
+      Topic: "Modo3 mohom",
+      Address: "hy25a",
+      Notes: "anta mal y3am lorem12fsd",
+      Area: "Inside",
+      Status: "done",
+    },
+    {
+      id: 11,
+      Date: "10-05-2024",
+      Time: "2:00",
+      Person: "Ahmed",
+      Topic: "Modo3 mohom",
+      Address: "hy25a",
+      Notes: "anta mal y3am lorem12fsd",
+      Area: "Inside",
+      Status: "not done",
+    },
+    {
+      id: 12,
+      Date: "10-05-2024",
+      Time: "2:00",
+      Person: "Saged",
+      Topic: "Modo3 mohom",
+      Address: "hy25a",
+      Notes: "anta mal y3am lorem12fsd",
+      Area: "Inside",
+      Status: "not done",
+    },
+    {
+      id: 13,
+      Date: "10-05-2024",
+      Time: "2:00",
+      Person: "Ali",
+      Topic: "Modo3 mohom",
+      Address: "hy25a",
+      Notes: "anta mal y3am lorem12fsd",
+      Area: "Inside",
+      Status: "done",
     },
   ]);
+
+  let [selectedId, setSelectId] = useState(null);
+
+  function rowContextMenu(x, y) {
+    $("#context-menu").css({ top: y, left: x, display: "block" });
+  }
   return (
     <>
       <div className="main">
@@ -82,24 +173,85 @@ export default function Meetings() {
           <h2 className="mt-4 mb-5" style={{ userSelect: "none" }}>
             Meetings
           </h2>
-          <div className="meetings ">
-            <Box>
+          <div className="meetingsConatiner p-5" style={{ width: "100%" }}>
+            <div className="meetings" style={{ height: 550, width: "100%" }}>
               <DataGrid
+                slotProps={{
+                  row: {
+                    onContextMenu: (e) => {
+                      e.preventDefault();
+                      rowContextMenu(e.pageX, e.pageY);
+                      setSelectId(e.currentTarget.getAttribute("data-id"));
+                    },
+                    style: { cursor: "context-menu" },
+                  },
+                }}
                 columns={[
-                  { field: "Date", headerClassName: "tableColumns" },
-                  { field: "Time", headerClassName: "tableColumns" },
-                  { field: "Person", headerClassName: "tableColumns" },
-                  { field: "Topic", headerClassName: "tableColumns" },
-                  { field: "Address", headerClassName: "tableColumns" },
-                  { field: "Notes", headerClassName: "tableColumns" },
-                  { field: "Area", headerClassName: "tableColumns" },
+                  {
+                    field: "Date",
+                    headerClassName: "tableColumns",
+                    align: "center",
+                    headerAlign: "center",
+                    width: 140,
+                  },
+                  {
+                    field: "Time",
+                    headerClassName: "tableColumns",
+                    align: "center",
+                    headerAlign: "center",
+                    width: 140,
+                  },
+                  {
+                    field: "Person",
+                    headerClassName: "tableColumns",
+                    align: "center",
+                    headerAlign: "center",
+                    width: 140,
+                  },
+                  {
+                    field: "Topic",
+                    headerClassName: "tableColumns",
+                    align: "center",
+                    headerAlign: "center",
+                    width: 140,
+                  },
+                  {
+                    field: "Address",
+                    headerClassName: "tableColumns",
+                    align: "center",
+                    headerAlign: "center",
+                    width: 140,
+                  },
+                  {
+                    field: "Area",
+                    headerClassName: "tableColumns",
+                    align: "center",
+                    headerAlign: "center",
+                    width: 140,
+                  },
+                  {
+                    field: "Status",
+                    headerClassName: "tableColumns",
+                    align: "center",
+                    headerAlign: "center",
+                    width: 140,
+                  },
+                  {
+                    field: "Notes",
+                    headerClassName: "tableColumns",
+                    align: "center",
+                    headerAlign: "center",
+                    width: 140,
+                  },
                 ]}
                 rows={meetingsRows}
                 sx={{
+                  overflow: "auto",
+                  maxHeight: "550px",
                   padding: "10px",
+
                   "& .MuiDataGrid-cell": {
                     color: "white",
-                    marginInline: "15px",
                     cursor: "pointer",
                   },
                   "& .MuiDataGrid-cell:focus": {
@@ -117,7 +269,29 @@ export default function Meetings() {
                     },
                 }}
               />
-            </Box>
+            </div>
+          </div>
+        </div>
+
+        <div id="context-menu">
+          <div className="item" onClick={() => console.log("Done", selectedId)}>
+            Done
+          </div>
+          <div
+            className="item"
+            onClick={() => {
+              console.log("Update", selectedId);
+            }}
+          >
+            Update
+          </div>
+          <div
+            className="item"
+            onClick={() => {
+              console.log("Delete", selectedId);
+            }}
+          >
+            Delete
           </div>
         </div>
       </div>
--- meeting-management/src/components/sidebar/Sidebar.jsx
+++ meeting-management/src/components/sidebar/Sidebar.jsx
@@ -10,15 +10,15 @@ const Sidebar = () => {
                 <img src={logo} alt="Logo" />
                 <div className="container">
                     <div className="header-side">
-                        <h6><a><span className="text-center"><i class="fa-solid fa-house"></i></span><span>Home</span></a></h6>
-                        <h6><a><span className="text-center"><i class="fa-regular fa-calendar-days"></i></span><span>Calendar</span></a></h6>
-                        <h6><a><span className="text-center"><i class="fa-regular fa-bell"></i></span><span>Notification</span></a></h6>
-                        <h6><a><span className="text-center"><i class="fa-solid fa-plus"></i></span><span>create meeting</span></a></h6>
-                        <h6><a><span className="text-center"><i class="fa-solid fa-handshake"></i></span><span>meetings</span></a></h6>
+                        <h6><a><span className="text-center"><i className="fa-solid fa-house"></i></span><span>Home</span></a></h6>
+                        <h6><a><span className="text-center"><i className="fa-regular fa-calendar-days"></i></span><span>Calendar</span></a></h6>
+                        <h6><a><span className="text-center"><i className="fa-regular fa-bell"></i></span><span>Notification</span></a></h6>
+                        <h6><a><span className="text-center"><i className="fa-solid fa-plus"></i></span><span>create meeting</span></a></h6>
+                        <h6><a><span className="text-center"><i className="fa-solid fa-handshake"></i></span><span>meetings</span></a></h6>
                     </div>
                     <div className="setting-side">
-                        <h6><a><i class="fa-solid fa-gear"></i><span>settings</span></a></h6>
-                        <h6><a><i class="fa-solid fa-arrow-right-to-bracket"></i><span>Logout</span></a></h6>
+                        <h6><a><i className="fa-solid fa-gear"></i><span>settings</span></a></h6>
+                        <h6><a><i className="fa-solid fa-arrow-right-to-bracket"></i><span>Logout</span></a></h6>
                     </div>
                 </div>
             </div>
