0477cf447c3432ba5824c9002fcd58d1ccf0b328
OmarKadryDhb <omardahab38@gmail.com>
2023-12-23 18:30:44 +0200

    login - signUp

--- meeting-management/src/components/sidebar/Sidebar.jsx
+++ meeting-management/src/components/sidebar/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import "./sidebar.css"
-import logo from '../../image/Logo small.png'
+import logo from '../../image/Logosmall.png'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
--- meeting-management/src/components/signup/Signup.jsx
+++ meeting-management/src/components/signup/Signup.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
+import './Signup.css'
+import Form from 'react-bootstrap/Form';
+import logoMed from '../../image/Logomedium.png'
 
 export default function Signup() {
-  return (
-    <div>
-      Signup
+  return <>
+   <div className='signup-main d-flex justify-content-center align-items-center'>
+    <div className="container">
+      <div className="signup-logo d-flex justify-content-center align-items-center mb-5">
+        <img src={logoMed} alt="" />
+      </div>
+      <div className="row">
+        <div className="col-md-5 m-auto">
+      <div className="ineer">
+        <div className="form">
+          <div className="name d-flex justify-content-between">
+            <input type="text" className='first-name form-control ' placeholder='First Name' />
+            <input type="text" className='last-name form-control' placeholder='Last Name'/>
+          </div>
+          <input type="text" className='user-name mt-3 d-flex justify-content-center form-control' placeholder='User Name'/>
+          <input type="text" className='user-name mt-3 d-flex justify-content-center form-control' placeholder='Password'/>
+          <input type="text" className='user-name mt-3 d-flex justify-content-center form-control' placeholder='Confirm Password'/>
+          <Form.Select aria-label="Default select example" className="role mt-3" arial>
+            <option>Role</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+          </Form.Select>
+        </div>
+        <div className="Signup-btn d-flex justify-content-center align-items-center mt-3">
+          <button className=''>Create account</button>
+        </div>
+      </div>
+      </div>
     </div>
-  )
+    </div>
+  </div>
+  </>
 }
