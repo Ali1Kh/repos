f9360c94c8e881262096ee6aed598a9b8b2be347
Ali Khaled <alielsaadany94@gmail.com>
2023-12-25 00:09:13 +0200

    gh-pages

--- meeting-management/src/index.js
+++ meeting-management/src/index.js
@@ -5,4 +5,4 @@ import App from './App';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />); 
