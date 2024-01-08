8a9d86796e51b1bceafc4379e57021d455969388
Ali Khaled <alielsaadany94@gmail.com>
2024-01-08 03:08:26 +0200

    ui handle

--- meeting-management/src/components/login/Login.jsx
+++ meeting-management/src/components/login/Login.jsx
@@ -10,29 +10,37 @@ export default function Login() {
     <>
       <div className="main">
         <div className="container p-5 text-center ">
-        <h2 className="mb-5 animate__animated animate__zoomIn BlackToWhite">
+          <h2 className="mb-5 animate__animated animate__zoomIn BlackToWhite">
             {t("Login.loginheader")}
           </h2>
-          <div className="ineer p-5 shadow">
-            <div className="form mb-4">
-              <input
-                type="text"
-                className="user-name mt-3 d-flex justify-content-center form-control"
-                placeholder={t("Login.username")}
-              />
-              <input
-                type="text"
-                className="user-name mt-3 d-flex justify-content-center form-control"
-                placeholder={t("Login.password")}
-              />
-            </div>
-            <p className="mt-2 px-3 text-start BlackToWhite">
-            {t("Login.signuphint")}<Link to={"/signup"}>{t("Login.signup")}</Link>
-            </p>
-            <div className="login-btn d-flex justify-content-center align-items-center mt-3">
-              <Link to={"/#"} className="">
-                {t("Login.button")}
-              </Link>
+          <div className="row justify-content-center">
+            <div className="col-md-7">
+              <div className="ineer p-5 shadow">
+                <div className="form mb-4">
+                  <input
+                    type="text"
+                    className="user-name mt-3 d-flex justify-content-center form-control"
+                    placeholder={t("Login.username")}
+                  />
+                  <input
+                    type="text"
+                    className="user-name mt-3 d-flex justify-content-center form-control"
+                    placeholder={t("Login.password")}
+                  />
+                </div>
+                <p
+                  style={{ fontSize: "14px" }}
+                  className="mt-2 px-3 text-start BlackToWhite"
+                >
+                  {t("Login.signuphint")}
+                  <Link to={"/signup"}>{t("Login.signup")}</Link>
+                </p>
+                <div className="login-btn d-flex justify-content-center align-items-center mt-3">
+                  <Link to={"/#"} className="">
+                    {t("Login.button")}
+                  </Link>
+                </div>
+              </div>
             </div>
           </div>
         </div>
--- meeting-management/src/components/signup/Signup.jsx
+++ meeting-management/src/components/signup/Signup.jsx
@@ -23,59 +23,65 @@ export default function Signup() {
           <h2 className="mb-5 animate__animated animate__zoomIn BlackToWhite">
             {t("signup.signupheader")}
           </h2>
-          <form className="ineer shadow p-5">
-            <div className="form">
-              <div className="name d-flex justify-content-between">
-                <input
-                  type="text"
-                  className="first-name form-control"
-                  placeholder={t("signup.firstName")}
-                />
-                {/* {initial.firstName.length < 4 || initial.firstName.length > 10 ? <div className='alert alert-danger'>Name must be from 4 to 10 charachters.</div> :"" }
-                 */}
-                <input
-                  type="text"
-                  className="last-name form-control"
-                  placeholder={t("signup.lastName")}
-                />
-                {/* {initial.lastName.length < 4 || initial.lastName.length > 10 ? <div className='alert alert-danger'>Name must be from 4 to 10 charachters.</div> :"" }
-                 */}
-              </div>
+          <div className="row justify-content-center">
+            <div className="col-md-9">
+              <form className="ineer shadow p-5">
+                <div className="form">
+                  <div className="name row gy-3">
+                    <div className="col-md-6">
+                      <input
+                        type="text"
+                        className="first-name w-100 form-control"
+                        placeholder={t("signup.firstName")}
+                      />
+                    </div>
 
-              <input
-                type="text"
-                className="user-name mt-3 d-flex justify-content-center form-control"
-                placeholder={t("signup.userName")}
-              />
-              <input
-                type="text"
-                className="password mt-3 d-flex justify-content-center form-control"
-                placeholder={t("signup.password")}
-              />
-              <input
-                type="text"
-                className="confirm-password mt-3 d-flex justify-content-center form-control"
-                placeholder={t("signup.confirmPassword")}
-              />
+                    <div className="col-md-6">
+                      <input
+                        type="text"
+                        className="last-name w-100 form-control"
+                        placeholder={t("signup.lastName")}
+                      />
+                    </div>
 
-              <Form.Select aria-label="Role" className="role mt-3" arial>
-                <option selected disabled>
-                  {t("signup.role")}
-                </option>
-                <option value="Manager">{t("signup.manager")}</option>
-                <option value="Secretary">{t("signup.sec")}</option>
-              </Form.Select>
+                  </div>
+
+                  <input
+                    type="text"
+                    className="user-name mt-3 d-flex justify-content-center form-control"
+                    placeholder={t("signup.userName")}
+                  />
+                  <input
+                    type="text"
+                    className="password mt-3 d-flex justify-content-center form-control"
+                    placeholder={t("signup.password")}
+                  />
+                  <input
+                    type="text"
+                    className="confirm-password mt-3 d-flex justify-content-center form-control"
+                    placeholder={t("signup.confirmPassword")}
+                  />
+
+                  <Form.Select aria-label="Role" className="role mt-3" arial>
+                    <option selected disabled>
+                      {t("signup.role")}
+                    </option>
+                    <option value="Manager">{t("signup.manager")}</option>
+                    <option value="Secretary">{t("signup.sec")}</option>
+                  </Form.Select>
+                </div>
+                <p style={{ fontSize: "14px" }} className="mt-2 text-start BlackToWhite px-3">
+                  {t("signup.loginHint")}{" "}
+                  <Link to={"/login"}>{t("signup.login")}</Link>
+                </p>
+                <div className="Signup-btn d-flex justify-content-center align-items-center mt-3">
+                  <button type="button" className="">
+                    {t("signup.signupheader")}
+                  </button>
+                </div>
+              </form>
             </div>
-            <p className="mt-2 text-start BlackToWhite px-3">
-              {t("signup.loginHint")}{" "}
-              <Link to={"/login"}>{t("signup.login")}</Link>
-            </p>
-            <div className="Signup-btn d-flex justify-content-center align-items-center mt-3">
-              <button type="button" className="">
-                {t("signup.signupheader")}
-              </button>
-            </div>
-          </form>
+          </div>
         </div>
       </div>
     </>
