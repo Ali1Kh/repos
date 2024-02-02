67cfb21f8f88b82835e2967d2d08bb766db63775
Ali Khaled <alielsaadany94@gmail.com>
2024-02-02 23:12:06 +0200

    folder

--- src/index.js
+++ src/index.js
@@ -7,4 +7,4 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
 import 'animate.css';
 import './i18n';
 import App from './App';
-ReactDOM.createRoot(document.getElementById('root')).render(<App />); 
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
