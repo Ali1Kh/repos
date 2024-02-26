cec0f29383091f4807b203f49c6674443fe53b47
Ali Khaled <alielsaadany94@gmail.com>
2024-02-26 21:29:48 +0200

    Get Manager Details

--- src/modules/manager/manager.controller.js
+++ src/modules/manager/manager.controller.js
@@ -1,6 +1,10 @@
 import { Manager } from "../../../DB/models/manager.model.js";
 
 export const getManagerDetails = async (req, res, next) => {
-  const manager = await Manager.findByPk(req.payload.id);
+  const manager = await Manager.findByPk(req.payload.id,{
+    attributes:{
+      exclude:["PassWord","resetCodeVerified","resetCode"]
+    }
+  });
   return res.json({ success: true, manager });
 };
