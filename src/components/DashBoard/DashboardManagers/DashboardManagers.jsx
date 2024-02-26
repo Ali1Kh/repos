4d1f8bed09a3b30bfcc907924dca68dbb818fa0b
OmarKadryDhb <omardahab38@gmail.com>
2024-02-26 22:49:47 +0200

    Managers Dashboard

--- src/components/DashBoard/DashboardManagers/DashboardManagers.jsx
+++ src/components/DashBoard/DashboardManagers/DashboardManagers.jsx
@@ -16,10 +16,10 @@ import { TailSpin } from 'react-loader-spinner';
 export default function DashBoardManagers() {
   const token = localStorage.getItem("token")
 
-  const { data, isLoading } = useQuery("getDashBoardSecertar", getDashBoardSecertar);
+  const { data, isLoading } = useQuery("getDashBoardManager", getDashBoardManager);
 
-  function getDashBoardSecertar() {
-    return axios.get("https://meetingss.onrender.com/dashboard/getAllSecretaries", {
+  function getDashBoardManager() {
+    return axios.get("https://meetingss.onrender.com/dashboard/getAllManagers", {
       headers: {
         token: token
       }
@@ -44,7 +44,7 @@ export default function DashBoardManagers() {
     <div className="main">
       <div className="container mt-5">
         <h1 className="container d-flex flex-column align-items-center justify-content-center p-4">
-          Secertaries
+        Managers
         </h1>
         <div className="row gy-3 p-5 pt-0">
           {isLoading ?
@@ -73,21 +73,21 @@ export default function DashBoardManagers() {
                       <Table stickyHeader aria-label="sticky table">
                         <TableHead>
                           <TableRow>
-                            <TableCell align="center">UserName</TableCell>
-                            <TableCell align="center">Name</TableCell>
-                            <TableCell align="center">Email</TableCell>
-                            <TableCell align="center">Accepted</TableCell>
-                            <TableCell align="center">Delete</TableCell>
+                            <TableCell align="center" className="fw-bold">UserName</TableCell>
+                            <TableCell align="center" className="fw-bold">Name</TableCell>
+                            <TableCell align="center" className="fw-bold">Email</TableCell>
+                            <TableCell align="center" className="fw-bold">Accepted</TableCell>
+                            <TableCell align="center" className="fw-bold">Delete</TableCell>
                           </TableRow>
                         </TableHead>
                         <TableBody>
                           {data
-                            ? data.data.secertaries?.map((secertarie, idx) => (
+                            ? data.data.managers?.map((manager, idx) => (
                               <TableRow hover tabIndex={-1} key={idx}>
-                                <TableCell align="center" component="th" scope="row">{secertarie.UserName}</TableCell>
-                                <TableCell align="center" component="th">{secertarie.first_name + " " + secertarie.last_name}</TableCell>
-                                <TableCell align="center" component="th">{secertarie.E_mail}</TableCell>
-                                <TableCell align="center" component="th">{secertarie.Accepted_Acc ? "Accept" : "Refused"}</TableCell>
+                                <TableCell align="center" component="th" scope="row">{manager.UserName}</TableCell>
+                                <TableCell align="center" component="th">{manager.first_name + " " + manager.last_name}</TableCell>
+                                <TableCell align="center" component="th">{manager.E_mail}</TableCell>
+                                <TableCell align="center" component="th">{manager.Accepted_Acc ? "Accept" : "Refused"}</TableCell>
                                 <TableCell align="center" component="th">
                                   <button align="center" className='btn btn-danger'>
                                     Delete
@@ -102,7 +102,7 @@ export default function DashBoardManagers() {
                     <TablePagination
                       rowsPerPageOptions={[5, 10, 25]}
                       component="div"
-                      count={data?.data.secertaries?.length}
+                      count={data?.data.managers?.length}
                       rowsPerPage={rowsPerPage}
                       page={page}
                       onPageChange={handleChangePage}
