aca637f4fca1922e2d942c568796f27bb89d1971
mustafa <museysalem06@gmail.com>
2024-02-27 03:21:43 +0200

    Acceptance

--- src/components/DashBoard/Acceptance/Acceptance.jsx
+++ src/components/DashBoard/Acceptance/Acceptance.jsx
@@ -6,46 +6,147 @@ import { TailSpin } from "react-loader-spinner";
 import { useQuery } from "react-query";
 
 const Acceptance = () => {
-    const [users, setUsers] = useState([
-        { username: 'Mustafasalem', name: 'Mustafa Salem', email: 'mustafa@gmail.com' },
-        { username: 'Ali', name: 'Ali soso', email: 'Ali@gmail.com' },
-    ]);
+    const token = localStorage.getItem("token");
 
-    const handleAccept = (email) => {
-        setUsers((prevUsers) => prevUsers.filter((user) => user.email !== email));
-        toast.success('Accepted!');
+    let [data, setData] = useState([]);
+
+    const getNotAcceptedAccounts = async () => {
+        try {
+            const response = await axios.get(
+                "https://meetingss.onrender.com/dashboard/getNotAcceptedSec",
+                {
+                    headers: {
+                        token: token,
+                    },
+                }
+            );
+            setData(response.data);
+            return response.data;
+        } catch (error) {
+            console.error("Error:", error);
+        }
     };
 
-    const handleRemove = (email) => {
-        setUsers((prevUsers) => prevUsers.filter((user) => user.email !== email));
-        toast.success('Removed!');
-    };
+    const { isLoading } = useQuery("getAcceptAccount", getNotAcceptedAccounts);
 
+    async function acceptAccount(id) {
+        try {
+            const response = await axios.post(
+                `https://meetingss.onrender.com/dashboard/acceptAcc/${id}`,
+                {},
+                {
+                    headers: {
+                        token: token,
+                    },
+                }
+            );
+            console.log(response);
+            if (response.data.success) {
+                toast.success("Accepted");
+                getNotAcceptedAccounts();
+            }
+        } catch (error) {
+            console.error("Error:", error);
+        }
+    }
+
+    const rejectAccount = async (id) => {
+        try {
+            const response = await axios.post(
+                `https://meetingss.onrender.com/dashboard/rejectAcc/${id}`,
+                {},
+                {
+                    headers: {
+                        token: token,
+                    },
+                }
+            );
+            console.log(response);
+
+            if (response.data.success) {
+                toast.success("Removed");
+                getNotAcceptedAccounts();
+            } else {
+                // Handle failure
+            }
+        } catch (error) {
+            console.error("Error:", error);
+        }
+    };
 
     return (
         <>
-            <div className='main'>
-                <div className='container mt-5'>
-                    <h1 className='container d-flex flex-column align-items-center justify-content-center p-4 fw-bold text-white'>Acceptance</h1>
-                    <div className='row header-table justify-content-center'>
-                        <div className="col">
-                            <h6>Username</h6>
+            <div className="main">
+                <div className="container mt-5">
+                    <h1 className="container d-flex flex-column align-items-center justify-content-center p-4">
+                        Acceptance
+                    </h1>
+                    {isLoading ? (
+                        <div
+                            className="d-flex justify-content-center align-items-center"
+                            style={{ height: "65vh" }}
+                        >
+                            <TailSpin
+                                visible={true}
+                                height="90"
+                                width="90"
+                                color="var(--sec-color)"
+                                ariaLabel="tail-spin-loading"
+                                radius="1"
+                                wrapperStyle={{}}
+                                wrapperClass=""
+                            />
                         </div>
-                        <div className="col">
-                            <h6>Name</h6>
+                    ) : (
+                        <div>
+                            <div className="row header-table justify-content-center">
+                                <div className="col">
+                                    <h6>Username</h6>
+                                </div>
+                                <div className="col">
+                                    <h6>Name</h6>
+                                </div>
+                                <div className="col">
+                                    <h6>Email</h6>
+                                </div>
+                                <div className="col">
+                                    <h6>Action</h6>
+                                </div>
+                            </div>
+                            <div className="row justify-content-center">
+                                {data
+                                    ? data.secertaries?.map((secretary, idx) => (
+                                        <div
+                                            key={idx}
+                                            className="row table justify-content-center"
+                                        >
+                                            <div className="col">{secretary.UserName}</div>
+                                            <div className="col">
+                                                {secretary.first_name + " " + secretary.last_name}
+                                            </div>
+                                            <div className="col">{secretary.E_mail}</div>
+                                            <div className="col-4">
+                                                <i
+                                                    className="fa-solid fa-trash deletAcc"
+                                                    onClick={() => {
+                                                        rejectAccount(secretary.secretary_id);
+                                                    }}
+                                                ></i>
+                                                <button
+                                                    className="btn accept-button"
+                                                    onClick={() => {
+                                                        acceptAccount(secretary.secretary_id);
+                                                    }}
+                                                >
+                                                    Accept
+                                                </button>
+                                            </div>
+                                        </div>
+                                    ))
+                                    : ""}
+                            </div>
                         </div>
-                        <div className="col">
-                            <h6>Email</h6>
-                        </div>
-                        <div className="col">
-                            <h6>Action</h6>
-                        </div>
-                    </div>
-                    <div className='row justify-content-center'>
-                        {users.map((user) => (
-                            <UserRow key={user.email} user={user} onAccept={handleAccept} onRemove={handleRemove} />
-                        ))}
-                    </div>
+                    )}
                 </div>
             </div>
         </>
