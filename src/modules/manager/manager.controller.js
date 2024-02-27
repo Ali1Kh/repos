df5eaa6ed2819121b717b4fb849c142536c5c8a0
Ali Khaled <alielsaadany94@gmail.com>
2024-02-28 01:54:04 +0200

    Fix

--- src/modules/manager/manager.controller.js
+++ src/modules/manager/manager.controller.js
@@ -1,10 +1,16 @@
 import { Manager } from "../../../DB/models/manager.model.js";
+import { Secertary } from "../../../DB/models/secertary.model.js";
 
 export const getManagerDetails = async (req, res, next) => {
-  const manager = await Manager.findByPk(req.payload.id,{
-    attributes:{
-      exclude:["PassWord","resetCodeVerified","resetCode"]
-    }
+  const manager = await Manager.findByPk(req.payload.id, {
+    attributes: {
+      exclude: ["PassWord", "resetCodeVerified", "resetCode"],
+    },
+    include: {
+      model: Secertary,
+      attributes: ["secretary_id"],
+      through: { attributes: [] }
+    },
   });
   return res.json({ success: true, manager });
 };
