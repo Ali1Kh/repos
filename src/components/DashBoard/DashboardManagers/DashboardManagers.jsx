52e719b891ee05df0b26cc324327f8f0622f2243
mustafa <museysalem06@gmail.com>
2024-02-29 01:36:34 +0200

    Dashboard

--- src/components/DashBoard/DashboardManagers/DashboardManagers.jsx
+++ src/components/DashBoard/DashboardManagers/DashboardManagers.jsx
@@ -5,7 +5,6 @@ import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
-import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
 import './DashboardManagers.css';
 import axios from 'axios';
@@ -54,17 +53,7 @@ export default function DashBoardManagers() {
       });
 };
 
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(10);
 
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(+event.target.value);
-    setPage(0);
-  };
 
   const [t] = useTranslation();
 
@@ -130,15 +119,6 @@ export default function DashBoardManagers() {
                         </TableBody>
                       </Table>
                     </TableContainer>
-                    <TablePagination
-                      rowsPerPageOptions={[5, 10, 25]}
-                      component="div"
-                      count={data?.managers?.length}
-                      rowsPerPage={rowsPerPage}
-                      page={page}
-                      onPageChange={handleChangePage}
-                      onRowsPerPageChange={handleChangeRowsPerPage}
-                    />
                   </Paper>
                 </div>
               </div>
