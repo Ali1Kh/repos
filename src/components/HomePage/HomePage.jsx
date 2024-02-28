ae98606e873613c8f2c94197db8219a3c9d9dc99
OmarKadryDhb <omardahab38@gmail.com>
2024-02-29 00:10:49 +0200

    search

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect , useState } from "react";
 import "./homePage.css";
 import MeetingDetails from "../manager/meetingDetails/meetingDetails.jsx";
 import { useTranslation } from "react-i18next";
@@ -73,6 +73,23 @@ export default function HomePage() {
 
   localStorage.setItem("colors", JSON.stringify(colors));
 
+  async function search(name) {
+    const {data} = await axios.get(`https://meetingss.onrender.com/meetings?person=${name}`, {
+      headers: {
+        token: authToken
+      }
+    })
+    if (data.success) { 
+      setData(data)
+    }
+  }
+
+  let searchName=$("#searchName");
+  searchName.keyup(function(){
+      search(searchName.val())
+      // console.log(data);
+  })
+
   return (
     <>
       <div className="main p-4 mt-5">
@@ -103,7 +120,7 @@ export default function HomePage() {
             <div className="row gy-3">
               <>
                 {data
-                  ? data.data.meetings?.map((meeting, idx) => (
+                  ? data.meetings?.map((meeting, idx) => (
                     <>
                       <div
                         key={idx}
