7205db9bc52f8b484ad3452e19988b2e44fe54a8
Ali Khaled <alielsaadany94@gmail.com>
2024-03-05 23:17:55 +0200

    handle dashboard

--- src/modules/dashboard/dashboard.controller.js
+++ src/modules/dashboard/dashboard.controller.js
@@ -8,13 +8,16 @@ import { asyncHandler } from "../../utils/asyncHandler.js";
 
 export const getAllMeetings = asyncHandler(async (req, res, next) => {
   let meetings = await sequelize.query(
-    `select Meetings.meeting_id,time,date,about,in_or_out,address, notes,addedBy,person,statues,Meetings.createdAt,Meetings.updatedAt,
+    `select Meetings.meeting_id,time,date,about,in_or_out,address, notes,person,statues,Meetings.createdAt,Meetings.updatedAt,
      attachmentId,attachmentLink,attachmentName,
      Manager.manager_id,CONCAT(first_name, ' ', last_name)  as 'Manager_Name',E_mail as 'Manager_Email',UserName as 'Manager_UserName' from Meetings
      join meeting_Manager on Meetings.meeting_id = meeting_Manager.meeting_id 
-     join Manager on meeting_Manager.manager_id = Manager.manager_id`
+     join Manager on meeting_Manager.manager_id = Manager.manager_id  
+      GROUP BY Meetings.meeting_id`,
+    {
+      model: Meetings,
+    }
   );
-
   return res.json({ success: true, count: meetings.length, meetings });
 });
 
