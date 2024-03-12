5fd9faf57740f1c6b5d743117ed17c70ce5c77b7
Ali Khaled <alielsaadany94@gmail.com>
2024-03-12 16:02:27 +0200

    -

--- src/modules/meatings/meetings.controller.js
+++ src/modules/meatings/meetings.controller.js
@@ -136,9 +136,14 @@ export const getManagerMeetingDetails = async (req, res, next) => {
 
 export const getMeetingManagers = async (req, res, next) => {
   let meetingManagers = await sequelize.query(
-    `select id,manager_id,meeting_Manager.meeting_id from meeting_Manager join Meetings on meeting_Manager.meeting_id = Meetings.meeting_id where manager_id = ${req.payload.id} and Meetings.date >= NOW()`,
+    `select id,manager_id,meeting_Manager.meeting_id from meeting_Manager join Meetings on meeting_Manager.meeting_id = Meetings.meeting_id where manager_id = ${
+      req.payload.id
+    } and Meetings.date >= '${new Date().toLocaleDateString("en-CA", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+    })}'`,
     { model: meeting_Manager }
   );
-
   return res.json({ success: true, meetingManagers });
 };
